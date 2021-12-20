0bfd1e37dfabb2ca94729e743beba417d7df43c9
Allen Titan <sutradhar019@gmail.com>
2021-12-20 06:07:34 +0600

    first commit

---
+++ resources/views/include/header.blade.php
@@ -0,0 +1,36 @@
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0,minimal-ui">
+<meta name="csrf-token" content="Qlcz6CqIksmArUKMEVITR4XkLns5ON6QBVJDlihK">
+<meta name="description"
+    content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
+<meta name="keywords"
+    content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
+<meta name="author" content="PIXINVENT">
+<title>Dashboard Ecommerce - Vuexy - Bootstrap HTML & Laravel admin template</title>
+<link rel="apple-touch-icon" href="{{asset('admin_assets/images/ico/apple-icon-120.html')}}">
+<link rel="shortcut icon" type="image/x-icon" href="{{asset('admin_assets/images/logo/favicon.ico')}}">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600"
+    rel="stylesheet">
+
+<!-- BEGIN: Vendor CSS-->
+<link rel="stylesheet" href="{{asset('admin_assets/vendors/css/vendors.min6144.css?id=cd237de63f2f3811a359')}}" />
+<link rel="stylesheet" href="{{asset('admin_assets/vendors/css/charts/apexcharts7fbc.css?id=3414033e38b96a20778e')}}">
+<link rel="stylesheet" href="{{asset('admin_assets/vendors/css/extensions/toastr.mindaf0.css?id=f46a48664f1939d7e483')}}">
+<!-- END: Vendor CSS-->
+
+<!-- BEGIN: Theme CSS-->
+<link rel="stylesheet" href="{{asset('admin_assets/css/core393f.css?id=b969fd28478dc1ee887a')}}" />
+
+<!-- BEGIN: Page CSS-->
+<link rel="stylesheet" href="{{asset('admin_assets/css/base/core/menu/menu-types/vertical-menu7ce3.css?id=d6fb64b9fc56ebf927d5')}}" />
+<link rel="stylesheet" href="{{asset('admin_assets/css/base/pages/dashboard-ecommerce20d4.css?id=658e290ea581278a06f3')}}">
+<link rel="stylesheet" href="{{asset('admin_assets/css/base/plugins/charts/chart-apexe27f.css?id=c3c3291d9ea78354a27b')}}">
+<link rel="stylesheet" href="{{asset('admin_assets/css/base/plugins/extensions/ext-component-toastr4069.css?id=626c9a1580e8f90cbff6')}}">
+
+<!-- laravel style -->
+<link rel="stylesheet" href="{{asset('admin_assets/css/overrides00ae.css?id=a3a7abd8c9ef0f541059')}}" />
+
+<!-- BEGIN: Custom CSS-->
+<link rel="stylesheet" href="{{asset('admin_assets/css/style2ddb.css?id=68b329da9893e34099c7')}}" />
+
---
+++ resources/views/include/scripts.blade.php
@@ -0,0 +1,35 @@
+<!-- BEGIN: Vendor JS-->
+<script src="{{asset('admin_assets/vendors/js/vendors.min9365.js?id=7dca1a1f6b86fd5d70ac')}}"></script>
+<!-- BEGIN Vendor JS-->
+
+<!-- BEGIN: Page Vendor JS-->
+<script src="{{asset('admin_assets/vendors/js/ui/jquery.sticky8a81.js?id=17f0788e54b9dc4eb93d')}}"></script>
+<script src="{{asset('admin_assets/vendors/js/charts/apexcharts.minbdb5.js?id=9c65c6ee3bb59eebe802')}}"></script>
+<script src="{{asset('admin_assets/vendors/js/extensions/toastr.min0ee4.js?id=4822c7d98e89e261b45d')}}"></script>
+<!-- END: Page Vendor JS-->
+
+<!-- BEGIN: Theme JS-->
+<script src="{{asset('admin_assets/js/core/app-menu54b0.js?id=0a3ed793d05ee5f6a9db')}}"></script>
+<script src="{{asset('admin_assets/js/core/app1778.js?id=6b6c2cc9a41161053158')}}"></script>
+
+<!-- custome scripts file for user -->
+<script src="{{asset('admin_assets/js/core/scripts56c1.js?id=22050af26ee69f8533fc')}}"></script>
+<script src="{{asset('admin_assets/js/scripts/customizer3ee2.js?id=00ad9be904861e76f046')}}"></script>
+<!-- END: Theme JS-->
+
+<!-- BEGIN: Page JS-->
+<script src="{{asset('admin_assets/js/scripts/pages/dashboard-ecommerced0a7.js?id=db47e79d28bf4f02ff4c')}}"></script>
+<!-- END: Page JS-->
+
+
+
+<script type="text/javascript">
+    $(window).on('load', function() {
+        if (feather) {
+            feather.replace({
+                width: 14,
+                height: 14
+            });
+        }
+    })
+</script>
